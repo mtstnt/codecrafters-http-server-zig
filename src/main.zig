fb74632e77ec4d8c50f2db135a4fb7fbe191eb8b
Matthew Sutanto <matthew.sutanto0612@gmail.com>
2024-07-13 17:39:02 +0700

    2nd challenge

--- src/main.zig
+++ src/main.zig
@@ -1,20 +1,18 @@
 const std = @import("std");
-// Uncomment this block to pass the first stage
 const net = std.net;
 
 pub fn main() !void {
-    const stdout = std.io.getStdOut().writer();
-
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    try stdout.print("Logs from your program will appear here!\n", .{});
-
-    // Uncomment this block to pass the first stage
     const address = try net.Address.resolveIp("127.0.0.1", 4221);
+
+    std.log.info("Listening to port 4221", .{});
     var listener = try address.listen(.{
         .reuse_address = true,
     });
     defer listener.deinit();
 
-    _ = try listener.accept();
-    try stdout.print("client connected!", .{});
+    var connection = try listener.accept();
+    _ = try connection.stream.writeAll("HTTP/1.1 200 OK\r\n\r\n");
+    connection.stream.close();
+
+    std.log.info("Exiting...", .{});
 }
