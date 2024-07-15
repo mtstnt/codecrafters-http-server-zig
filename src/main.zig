e83924459919cc57e6fc1e3801f849ae97554118
Matthew Sutanto <matthew.sutanto0612@gmail.com>
2024-07-15 20:07:39 +0700

    use multithread

--- src/main.zig
+++ src/main.zig
@@ -43,52 +43,61 @@ pub fn main() !void {
 
     while (true) {
         std.debug.print("Listening for requests...\n", .{});
-        var connection = try listener.accept();
+        const connection = try listener.accept();
 
-        std.debug.print("Request received.\n", .{});
-
-        const buffer = try Allocator.alloc(u8, BUFFER_SIZE);
-        defer Allocator.free(buffer);
-
-        // Reset the allocated buffer to zero values.
-        @memset(buffer, 0);
-
-        _ = try connection.stream.read(buffer);
-
-        const request = try httpRequest.parse(Allocator, buffer);
-
-        const routeKey = try matchRoute(request.path);
-        std.log.info("Route key: {s}", .{routeKey});
-
-        var response: []u8 = undefined;
-        if (strings.equals(routeKey, "ROOT")) {
-            const response_str = "HTTP/1.1 200 OK\r\n\r\n";
-            response = try Allocator.alloc(u8, response_str.len);
-            std.mem.copyForwards(u8, response, response_str);
-        } else if (strings.equals(routeKey, "ECHO")) {
-            const split_results = try strings.split(Allocator, request.path, "/");
-            defer split_results.deinit();
-
-            const path_parts = split_results.items;
-            const str = path_parts[1];
-            const format = "HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length:{d}\r\n\r\n{s}";
-            response = try std.fmt.allocPrint(Allocator, format, .{ str.len, str });
-        } else if (strings.equals(routeKey, "USER_AGENT")) {
-            const header_value = request.headers.get("User-Agent").?;
-            const format = "HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length:{d}\r\n\r\n{s}";
-            response = try std.fmt.allocPrint(Allocator, format, .{ header_value.len, header_value });
-        } else {
-            const response_str = "HTTP/1.1 404 Not Found\r\n\r\n";
-            response = try Allocator.alloc(u8, response_str.len);
-            std.mem.copyForwards(u8, response, response_str);
-        }
-        defer Allocator.free(response);
-
-        try connection.stream.writeAll(response);
-        connection.stream.close();
+        var handle = try std.Thread.spawn(
+            .{ .allocator = Allocator },
+            handleRequest,
+            .{connection},
+        );
+        handle.detach();
     }
 
     std.log.info("Exiting...", .{});
+}
+
+fn handleRequest(connection: std.net.Server.Connection) !void {
+    std.debug.print("Request received.\n", .{});
+
+    const buffer = try Allocator.alloc(u8, BUFFER_SIZE);
+    defer Allocator.free(buffer);
+
+    // Reset the allocated buffer to zero values.
+    @memset(buffer, 0);
+
+    _ = try connection.stream.read(buffer);
+
+    const request = try httpRequest.parse(Allocator, buffer);
+
+    const routeKey = try matchRoute(request.path);
+    std.log.info("Route key: {s}", .{routeKey});
+
+    var response: []u8 = undefined;
+    if (strings.equals(routeKey, "ROOT")) {
+        const response_str = "HTTP/1.1 200 OK\r\n\r\n";
+        response = try Allocator.alloc(u8, response_str.len);
+        std.mem.copyForwards(u8, response, response_str);
+    } else if (strings.equals(routeKey, "ECHO")) {
+        const split_results = try strings.split(Allocator, request.path, "/");
+        defer split_results.deinit();
+
+        const path_parts = split_results.items;
+        const str = path_parts[1];
+        const format = "HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length:{d}\r\n\r\n{s}";
+        response = try std.fmt.allocPrint(Allocator, format, .{ str.len, str });
+    } else if (strings.equals(routeKey, "USER_AGENT")) {
+        const header_value = request.headers.get("User-Agent").?;
+        const format = "HTTP/1.1 200 OK\r\nContent-Type:text/plain\r\nContent-Length:{d}\r\n\r\n{s}";
+        response = try std.fmt.allocPrint(Allocator, format, .{ header_value.len, header_value });
+    } else {
+        const response_str = "HTTP/1.1 404 Not Found\r\n\r\n";
+        response = try Allocator.alloc(u8, response_str.len);
+        std.mem.copyForwards(u8, response, response_str);
+    }
+    defer Allocator.free(response);
+
+    try connection.stream.writeAll(response);
+    connection.stream.close();
 }
 
 test "strSplit" {
